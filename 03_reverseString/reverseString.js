cd060abb43a546eddbad7df28c3c6afd0057c569
Rayden Sia <85201523+raydensia@users.noreply.github.com>
2023-03-14 23:21:56 +0800

    complete exercise 01, 02, 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
 
+    // let reversedString = [];
+    // let length = string.length - 1;
+    // string = string.split("");
+
+    // for (let i = length; i >= 0; i--) {
+    //     reversedString.push(string[i]);
+    // }
+
+    // return reversedString.join("");
 };
 
 // Do not edit below this line
